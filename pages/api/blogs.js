e666dc3c8d174c20f9dd4eeb24e0eb726e146c4b
Anas Dew <mailtoanas@duck.com>
2023-05-27 11:05:23 +0530

    fetch all blogs on blogs page

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,12 +2,24 @@
 
 import * as fs from 'fs';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+    let data = await fs.promises.readdir("blog_data")
+    let myblog;
+    let allBlogs = [];
 
-    fs.readdir(`blog_data`, (err, data) => {
-        if (err) {
-            return res.status(500).json({ error: err.errno })
-        }
-        res.status(200).json(data);
-    })
+    for (let index = 0; index < data.length; index++) {
+        const blogItem = data[index];
+        myblog = await fs.promises.readFile(('blog_data/' + blogItem), 'utf-8')
+        allBlogs.push(JSON.parse(myblog))
+        
+    }
+    res.status(200).json(allBlogs)
+
+    // ----------------
+    // fs.readdir(`blog_data`, (err, data) => {
+    //     if (err) {
+    //         return res.status(500).json({ error: err.errno })
+    //     }
+    //     res.status(200).json(data);
+    // })
 }
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,30 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
+import axios from 'axios'
 const inter = Inter({ subsets: ['latin'] })
 const Blogs = () => {
+    const [Blogs, setBlogs] = useState([])
+    useEffect(() => {
+        axios.get('/api/blogs').then((res) => {
+            console.log(res.data);
+            setBlogs(res.data)
+        })
+    }, [])
+
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
         >
             <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <Link
-                    href="/post/how-to-center-a-div"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                {Blogs.map((blogItem) => {
+                    return <Link
+                        href={`/post/${blogItem.slug}`}
+                        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
 
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        How to center a div{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        For noobs.
-                    </p>
-                </Link>
-
+                        rel="noopener noreferrer"
+                    >
+                        <h2 className={`mb-3 text-2xl font-semibold`}>
+                            {blogItem.title}{' '}
+                            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                                -&gt;
+                            </span>
+                        </h2>
+                        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                            {blogItem.content.slice(0, 60)}...
+                        </p>
+                    </Link>
+                })}
             </div>
         </main>
     )
